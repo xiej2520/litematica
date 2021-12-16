5197775204aba48cca9fa924a70756565e8d64cb
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2021-12-15 19:42:37 -0600

    Fix the per-chunk pasting not restoring any scheduled ticks

--- src/main/java/fi/dy/masa/litematica/schematic/LitematicaSchematic.java
+++ src/main/java/fi/dy/masa/litematica/schematic/LitematicaSchematic.java
@@ -888,6 +888,18 @@ public class LitematicaSchematic
         return this.entities.get(regionName);
     }
 
+    @Nullable
+    public Map<BlockPos, ScheduledTick<Block>> getScheduledBlockTicksForRegion(String regionName)
+    {
+        return this.pendingBlockTicks.get(regionName);
+    }
+
+    @Nullable
+    public Map<BlockPos, ScheduledTick<Fluid>> getScheduledFluidTicksForRegion(String regionName)
+    {
+        return this.pendingFluidTicks.get(regionName);
+    }
+
     private CompoundTag writeToNBT()
     {
         CompoundTag nbt = new CompoundTag();
