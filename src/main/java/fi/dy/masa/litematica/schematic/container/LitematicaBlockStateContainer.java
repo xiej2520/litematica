hi5ilay/gitmatica

Add support for loading the Sponge schematics
Also allow persistent placements for non-Litematica schematics
(the schematic is loaded from the correct file, and just re-converted
any time it's being loaded).

src/main/java/fi/dy/masa/litematica/schematic/container/LitematicaBlockStateContainer.java
@@ -4,8 +4,10 @@ import javax.annotation.Nullable;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.nbt.NbtList;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
+import io.netty.buffer.Unpooled;
 
 public class LitematicaBlockStateContainer implements ILitematicaBlockStatePaletteResizer
 {
@@ -19,10 +21,17 @@ public class LitematicaBlockStateContainer implements ILitematicaBlockStatePalet
     protected final int sizeLayer;
     protected final long totalVolume;
     protected int bits;
+    /** Note: This is currently only used for the temporary Sponge schematic support */
+    protected long[] blockCounts = new long[0];
 
     public LitematicaBlockStateContainer(int sizeX, int sizeY, int sizeZ)
     {
         this(sizeX, sizeY, sizeZ, 2, null);
+    }
+
+    public LitematicaBlockStateContainer(Vec3i size, int bits, @Nullable long[] backingLongArray)
+    {
+        this(size.getX(), size.getY(), size.getZ(), bits, backingLongArray);
     }
 
     public LitematicaBlockStateContainer(int sizeX, int sizeY, int sizeZ, int bits, @Nullable long[] backingLongArray)
@@ -45,6 +54,11 @@ public class LitematicaBlockStateContainer implements ILitematicaBlockStatePalet
     public LitematicaBitArray getArray()
     {
         return this.storage;
+    }
+
+    public long[] getBlockCounts()
+    {
+        return this.blockCounts;
     }
 
     public BlockState get(int x, int y, int z)
@@ -136,5 +150,45 @@ public class LitematicaBlockStateContainer implements ILitematicaBlockStatePalet
         LitematicaBlockStateContainer container = new LitematicaBlockStateContainer(size.getX(), size.getY(), size.getZ(), bits, blockStates);
         container.palette.readFromNBT(palette);
         return container;
+    }
+
+    @Nullable
+    public static LitematicaBlockStateContainer createContainer(int paletteSize, byte[] blockData, Vec3i size)
+    {
+        int bits = Math.max(2, Integer.SIZE - Integer.numberOfLeadingZeros(paletteSize - 1));
+        SpongeBlockstateConverterResults results = convertVarIntByteArrayToPackedLongArray(size, bits, blockData);
+        LitematicaBlockStateContainer container = new LitematicaBlockStateContainer(size, bits, results.backingArray);
+        //container.palette = createPalette(bits, container);
+        container.blockCounts = results.blockCounts;
+        return container;
+    }
+
+    public static SpongeBlockstateConverterResults convertVarIntByteArrayToPackedLongArray(Vec3i size, int bits, byte[] blockStates)
+    {
+        int volume = size.getX() * size.getY() * size.getZ();
+        LitematicaBitArray bitArray = new LitematicaBitArray(bits, volume);
+        PacketByteBuf buf = new PacketByteBuf(Unpooled.wrappedBuffer(blockStates));
+        long[] blockCounts = new long[1 << bits];
+
+        for (int i = 0; i < volume; ++i)
+        {
+            int id = buf.readVarInt();
+            bitArray.setAt(i, id);
+            ++blockCounts[id];
+        }
+
+        return new SpongeBlockstateConverterResults(bitArray.getBackingLongArray(), blockCounts);
+    }
+
+    public static class SpongeBlockstateConverterResults
+    {
+        public final long[] backingArray;
+        public final long[] blockCounts;
+
+        protected SpongeBlockstateConverterResults(long[] backingArray, long[] blockCounts)
+        {
+            this.backingArray = backingArray;
+            this.blockCounts = blockCounts;
+        }
     }
 }

src/main/java/fi/dy/masa/litematica/util/NbtUtils.java
@@ -6,10 +6,52 @@ import java.io.FileInputStream;
 import javax.annotation.Nullable;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtIo;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import fi.dy.masa.litematica.Litematica;
+import fi.dy.masa.malilib.util.Constants;
 
 public class NbtUtils
 {
+    @Nullable
+    public static BlockPos readBlockPosFromArrayTag(NbtCompound tag, String tagName)
+    {
+        if (tag.contains(tagName, Constants.NBT.TAG_INT_ARRAY))
+        {
+            int[] pos = tag.getIntArray("Pos");
+
+            if (pos.length == 3)
+            {
+                return new BlockPos(pos[0], pos[1], pos[2]);
+            }
+        }
+
+        return null;
+    }
+
+    @Nullable
+    public static Vec3d readVec3dFromListTag(@Nullable NbtCompound tag)
+    {
+        return readVec3dFromListTag(tag, "Pos");
+    }
+
+    @Nullable
+    public static Vec3d readVec3dFromListTag(@Nullable NbtCompound tag, String tagName)
+    {
+        if (tag != null && tag.contains(tagName, Constants.NBT.TAG_LIST))
+        {
+            NbtList tagList = tag.getList(tagName, Constants.NBT.TAG_DOUBLE);
+
+            if (tagList.getHeldType() == Constants.NBT.TAG_DOUBLE && tagList.size() == 3)
+            {
+                return new Vec3d(tagList.getDouble(0), tagList.getDouble(1), tagList.getDouble(2));
+            }
+        }
+
+        return null;
+    }
+
     @Nullable
     public static NbtCompound readNbtFromFile(File file)
     {

xiej2520/litematica

src/main/java/fi/dy/masa/litematica/schematic/container/LitematicaBlockStateContainer.java
@@ -4,8 +4,10 @@ import javax.annotation.Nullable;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.util.PacketByteBuf;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
+import io.netty.buffer.Unpooled;
 
 public class LitematicaBlockStateContainer implements ILitematicaBlockStatePaletteResizer
 {
@@ -17,10 +19,17 @@ public class LitematicaBlockStateContainer implements ILitematicaBlockStatePalet
     protected final int sizeZ;
     protected final int sizeLayer;
     protected int bits;
+    /** Note: This is currently only used for the temporary Sponge schematic support */
+    protected long[] blockCounts = new long[0];
 
     public LitematicaBlockStateContainer(int sizeX, int sizeY, int sizeZ)
     {
         this(sizeX, sizeY, sizeZ, 2, null);
+    }
+
+    public LitematicaBlockStateContainer(Vec3i size, int bits, @Nullable long[] backingLongArray)
+    {
+        this(size.getX(), size.getY(), size.getZ(), bits, backingLongArray);
     }
 
     public LitematicaBlockStateContainer(int sizeX, int sizeY, int sizeZ, int bits, @Nullable long[] backingLongArray)
@@ -41,6 +50,11 @@ public class LitematicaBlockStateContainer implements ILitematicaBlockStatePalet
     public LitematicaBitArray getArray()
     {
         return this.storage;
+    }
+
+    public long[] getBlockCounts()
+    {
+        return this.blockCounts;
     }
 
     public BlockState get(int x, int y, int z)
@@ -132,5 +146,45 @@ public class LitematicaBlockStateContainer implements ILitematicaBlockStatePalet
         LitematicaBlockStateContainer container = new LitematicaBlockStateContainer(size.getX(), size.getY(), size.getZ(), bits, blockStates);
         container.palette.readFromNBT(palette);
         return container;
+    }
+
+    @Nullable
+    public static LitematicaBlockStateContainer createContainer(int paletteSize, byte[] blockData, Vec3i size)
+    {
+        int bits = Math.max(2, Integer.SIZE - Integer.numberOfLeadingZeros(paletteSize - 1));
+        SpongeBlockstateConverterResults results = convertVarIntByteArrayToPackedLongArray(size, bits, blockData);
+        LitematicaBlockStateContainer container = new LitematicaBlockStateContainer(size, bits, results.backingArray);
+        //container.palette = createPalette(bits, container);
+        container.blockCounts = results.blockCounts;
+        return container;
+    }
+
+    public static SpongeBlockstateConverterResults convertVarIntByteArrayToPackedLongArray(Vec3i size, int bits, byte[] blockStates)
+    {
+        int volume = size.getX() * size.getY() * size.getZ();
+        LitematicaBitArray bitArray = new LitematicaBitArray(bits, volume);
+        PacketByteBuf buf = new PacketByteBuf(Unpooled.wrappedBuffer(blockStates));
+        long[] blockCounts = new long[1 << bits];
+
+        for (int i = 0; i < volume; ++i)
+        {
+            int id = buf.readVarInt();
+            bitArray.setAt(i, id);
+            ++blockCounts[id];
+        }
+
+        return new SpongeBlockstateConverterResults(bitArray.getBackingLongArray(), blockCounts);
+    }
+
+    public static class SpongeBlockstateConverterResults
+    {
+        public final long[] backingArray;
+        public final long[] blockCounts;
+
+        protected SpongeBlockstateConverterResults(long[] backingArray, long[] blockCounts)
+        {
+            this.backingArray = backingArray;
+            this.blockCounts = blockCounts;
+        }
     }
 }

src/main/java/fi/dy/masa/litematica/util/NbtUtils.java
@@ -7,10 +7,52 @@ import javax.annotation.Nullable;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtIo;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import fi.dy.masa.litematica.Litematica;
+import fi.dy.masa.malilib.util.Constants;
 
 public class NbtUtils
 {
+    @Nullable
+    public static BlockPos readBlockPosFromArrayTag(CompoundTag tag, String tagName)
+    {
+        if (tag.contains(tagName, Constants.NBT.TAG_INT_ARRAY))
+        {
+            int[] pos = tag.getIntArray("Pos");
+
+            if (pos.length == 3)
+            {
+                return new BlockPos(pos[0], pos[1], pos[2]);
+            }
+        }
+
+        return null;
+    }
+
+    @Nullable
+    public static Vec3d readVec3dFromListTag(@Nullable CompoundTag tag)
+    {
+        return readVec3dFromListTag(tag, "Pos");
+    }
+
+    @Nullable
+    public static Vec3d readVec3dFromListTag(@Nullable CompoundTag tag, String tagName)
+    {
+        if (tag != null && tag.contains(tagName, Constants.NBT.TAG_LIST))
+        {
+            ListTag tagList = tag.getList(tagName, Constants.NBT.TAG_DOUBLE);
+
+            if (tagList.getElementType() == Constants.NBT.TAG_DOUBLE && tagList.size() == 3)
+            {
+                return new Vec3d(tagList.getDouble(0), tagList.getDouble(1), tagList.getDouble(2));
+            }
+        }
+
+        return null;
+    }
+
     @Nullable
     public static CompoundTag readNbtFromFile(File file)
     {