hi5ilay/gitmatica

Easy Place: Trace to the furthest schematic block and place that first

src/main/java/fi/dy/masa/litematica/util/InventoryUtils.java
@@ -2,6 +2,7 @@ package fi.dy.masa.litematica.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
@@ -9,7 +10,12 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
+import net.minecraft.util.Hand;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import fi.dy.masa.litematica.config.Configs;
+import fi.dy.masa.malilib.gui.GuiBase;
 
 public class InventoryUtils
 {
@@ -84,6 +90,56 @@ public class InventoryUtils
                 {
                     fi.dy.masa.malilib.util.InventoryUtils.swapItemToMainHand(stack.copy(), mc);
                 }
+            }
+        }
+    }
+
+    public static void schematicWorldPickBlock(ItemStack stack, BlockPos pos,
+                                               World schematicWorld, MinecraftClient mc)
+    {
+        if (stack.isEmpty() == false)
+        {
+            PlayerInventory inv = mc.player.getInventory();
+            stack = stack.copy();
+
+            if (EntityUtils.isCreativeMode(mc.player))
+            {
+                BlockEntity te = schematicWorld.getBlockEntity(pos);
+
+                // The creative mode pick block with NBT only works correctly
+                // if the server world doesn't have a TileEntity in that position.
+                // Otherwise it would try to write whatever that TE is into the picked ItemStack.
+                if (GuiBase.isCtrlDown() && te != null && mc.world.isAir(pos))
+                {
+                    ItemUtils.storeTEInStack(stack, te);
+                }
+
+                setPickedItemToHand(stack, mc);
+                mc.interactionManager.clickCreativeStack(mc.player.getStackInHand(Hand.MAIN_HAND), 36 + inv.selectedSlot);
+
+                //return true;
+            }
+            else
+            {
+                int slot = inv.getSlotWithStack(stack);
+                boolean shouldPick = inv.selectedSlot != slot;
+
+                if (shouldPick && slot != -1)
+                {
+                    setPickedItemToHand(stack, mc);
+                }
+                else if (slot == -1 && Configs.Generic.PICK_BLOCK_SHULKERS.getBooleanValue())
+                {
+                    slot = findSlotWithBoxWithItem(mc.player.playerScreenHandler, stack, false);
+
+                    if (slot != -1)
+                    {
+                        ItemStack boxStack = mc.player.playerScreenHandler.slots.get(slot).getStack();
+                        setPickedItemToHand(boxStack, mc);
+                    }
+                }
+
+                //return shouldPick == false || canPick;
             }
         }
     }

src/main/java/fi/dy/masa/litematica/util/RayTraceUtils.java
@@ -437,28 +437,37 @@ public class RayTraceUtils
     }
 
     @Nullable
-    public static BlockPos getFurthestSchematicWorldTrace(World worldClient, Entity entity, double maxRange)
+    public static BlockPos getFurthestSchematicWorldBlockBeforeVanilla(World worldClient,
+                                                                       Entity entity,
+                                                                       double maxRange,
+                                                                       boolean requireVanillaBlockBehind)
     {
         Vec3d eyesPos = entity.getCameraPosVec(1f);
         Vec3d rangedLookRot = entity.getRotationVec(1f).multiply(maxRange);
         Vec3d lookEndPos = eyesPos.add(rangedLookRot);
 
+        @Nullable BlockPos closestVanillaPos = null;
+        @Nullable Direction side = null;
+        double closestVanilla = -1.0;
+
         HitResult traceVanilla = getRayTraceFromEntity(worldClient, entity, false, maxRange);
 
-        if (traceVanilla.getType() != HitResult.Type.BLOCK)
+        if (traceVanilla.getType() == HitResult.Type.BLOCK)
+        {
+            closestVanilla = traceVanilla.getPos().squaredDistanceTo(eyesPos);
+            BlockHitResult vanillaHitResult = (BlockHitResult) traceVanilla;
+            side = vanillaHitResult.getSide();
+            closestVanillaPos = vanillaHitResult.getBlockPos();
+        }
+        else if (requireVanillaBlockBehind)
         {
             return null;
         }
 
-        final double closestVanilla = traceVanilla.getPos().squaredDistanceTo(eyesPos);
-
-        BlockHitResult vanillaBlockHit = (BlockHitResult) traceVanilla;
-        Direction side = vanillaBlockHit.getSide();
-        BlockPos closestVanillaPos = vanillaBlockHit.getBlockPos();
         World worldSchematic = SchematicWorldHandler.getSchematicWorld();
         List<BlockHitResult> list = rayTraceBlocksToList(worldSchematic, eyesPos, lookEndPos, RaycastContext.FluidHandling.NONE, false, false, true, 200);
         BlockHitResult furthestTrace = null;
-        double furthestDist = -1D;
+        double furthestDist = -1.0;
 
         if (list.isEmpty() == false)
         {
@@ -466,8 +475,9 @@ public class RayTraceUtils
             {
                 double dist = trace.getPos().squaredDistanceTo(eyesPos);
 
-                if ((furthestDist < 0 || dist > furthestDist) && (dist < closestVanilla || closestVanilla < 0) &&
-                     trace.getBlockPos().equals(closestVanillaPos) == false)
+                if ((furthestDist < 0 || dist > furthestDist) &&
+                    (dist < closestVanilla || closestVanilla < 0) &&
+                    trace.getBlockPos().equals(closestVanillaPos) == false)
                 {
                     furthestDist = dist;
                     furthestTrace = trace;
@@ -487,7 +497,7 @@ public class RayTraceUtils
         // Note that this method is only used for the "pickBlockLast" type
         // of pick blocking, not for the "first" variant, where this would
         // probably be annoying if you want to pick block the client world block.
-        if (furthestTrace == null)
+        if (furthestTrace == null && side != null && closestVanillaPos != null)
         {
             BlockPos pos = closestVanillaPos.offset(side);
             LayerRange layerRange = DataManager.getRenderLayerRange();
@@ -501,6 +511,56 @@ public class RayTraceUtils
         }
 
         return furthestTrace != null ? furthestTrace.getBlockPos() : null;
+    }
+
+    @Nullable
+    public static RayTraceWrapper getFurthestSchematicWorldTraceBeforeVanilla(World worldClient,
+                                                                              Entity entity,
+                                                                              double maxRange)
+    {
+        Vec3d eyesPos = entity.getCameraPosVec(1f);
+        Vec3d rangedLookRot = entity.getRotationVec(1f).multiply(maxRange);
+        Vec3d lookEndPos = eyesPos.add(rangedLookRot);
+
+        @Nullable BlockPos closestVanillaPos = null;
+        double closestVanilla = -1.0;
+
+        HitResult traceVanilla = getRayTraceFromEntity(worldClient, entity, false, maxRange);
+
+        if (traceVanilla.getType() == HitResult.Type.BLOCK)
+        {
+            closestVanilla = traceVanilla.getPos().squaredDistanceTo(eyesPos);
+            BlockHitResult vanillaHitResult = (BlockHitResult) traceVanilla;
+            closestVanillaPos = vanillaHitResult.getBlockPos();
+        }
+
+        World worldSchematic = SchematicWorldHandler.getSchematicWorld();
+        List<BlockHitResult> list = rayTraceBlocksToList(worldSchematic, eyesPos, lookEndPos, RaycastContext.FluidHandling.NONE, false, false, true, 200);
+        BlockHitResult furthestTrace = null;
+        double furthestDist = -1.0;
+
+        if (list.isEmpty() == false)
+        {
+            for (BlockHitResult trace : list)
+            {
+                double dist = trace.getPos().squaredDistanceTo(eyesPos);
+
+                if ((furthestDist < 0 || dist > furthestDist) &&
+                    (dist < closestVanilla || closestVanilla < 0) &&
+                    trace.getBlockPos().equals(closestVanillaPos) == false)
+                {
+                    furthestDist = dist;
+                    furthestTrace = trace;
+                }
+
+                if (closestVanilla >= 0 && dist > closestVanilla)
+                {
+                    break;
+                }
+            }
+        }
+
+        return furthestTrace != null ? new RayTraceWrapper(HitType.SCHEMATIC_BLOCK, furthestTrace) : null;
     }
 
     @Nonnull

xiej2520/litematica

src/main/java/fi/dy/masa/litematica/util/InventoryUtils.java
@@ -2,6 +2,10 @@ package fi.dy.masa.litematica.util;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import fi.dy.masa.litematica.config.Configs;
+import fi.dy.masa.malilib.gui.GuiBase;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
@@ -11,6 +15,9 @@ import net.minecraft.container.PlayerContainer;
 import net.minecraft.container.Container;
 import net.minecraft.container.Slot;
 import net.minecraft.util.DefaultedList;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class InventoryUtils
 {
@@ -85,6 +92,56 @@ public class InventoryUtils
                 {
                     fi.dy.masa.malilib.util.InventoryUtils.swapItemToMainHand(stack.copy(), mc);
                 }
+            }
+        }
+    }
+
+    public static void schematicWorldPickBlock(ItemStack stack, BlockPos pos,
+                                               World schematicWorld, MinecraftClient mc)
+    {
+        if (stack.isEmpty() == false)
+        {
+            PlayerInventory inv = mc.player.inventory;
+            stack = stack.copy();
+
+            if (mc.player.abilities.creativeMode)
+            {
+                BlockEntity te = schematicWorld.getBlockEntity(pos);
+
+                // The creative mode pick block with NBT only works correctly
+                // if the server world doesn't have a TileEntity in that position.
+                // Otherwise it would try to write whatever that TE is into the picked ItemStack.
+                if (GuiBase.isCtrlDown() && te != null && mc.world.isAir(pos))
+                {
+                    ItemUtils.storeTEInStack(stack, te);
+                }
+
+                setPickedItemToHand(stack, mc);
+                mc.interactionManager.clickCreativeStack(mc.player.getStackInHand(Hand.MAIN_HAND), 36 + inv.selectedSlot);
+
+                //return true;
+            }
+            else
+            {
+                int slot = inv.getSlotWithStack(stack);
+                boolean shouldPick = inv.selectedSlot != slot;
+
+                if (shouldPick && slot != -1)
+                {
+                    setPickedItemToHand(stack, mc);
+                }
+                else if (slot == -1 && Configs.Generic.PICK_BLOCK_SHULKERS.getBooleanValue())
+                {
+                    slot = findSlotWithBoxWithItem(mc.player.playerContainer, stack, false);
+
+                    if (slot != -1)
+                    {
+                        ItemStack boxStack = mc.player.playerContainer.slots.get(slot).getStack();
+                        setPickedItemToHand(boxStack, mc);
+                    }
+                }
+
+                //return shouldPick == false || canPick;
             }
         }
     }

src/main/java/fi/dy/masa/litematica/util/RayTraceUtils.java
@@ -437,28 +437,37 @@ public class RayTraceUtils
     }
 
     @Nullable
-    public static BlockPos getFurthestSchematicWorldTrace(World worldClient, Entity entity, double maxRange)
+    public static BlockPos getFurthestSchematicWorldBlockBeforeVanilla(World worldClient,
+                                                                       Entity entity,
+                                                                       double maxRange,
+                                                                       boolean requireVanillaBlockBehind)
     {
         Vec3d eyesPos = entity.getCameraPosVec(1f);
         Vec3d rangedLookRot = entity.getRotationVec(1f).multiply(maxRange);
         Vec3d lookEndPos = eyesPos.add(rangedLookRot);
 
+        @Nullable BlockPos closestVanillaPos = null;
+        @Nullable Direction side = null;
+        double closestVanilla = -1.0;
+
         HitResult traceVanilla = getRayTraceFromEntity(worldClient, entity, false, maxRange);
 
-        if (traceVanilla.getType() != HitResult.Type.BLOCK)
+        if (traceVanilla.getType() == HitResult.Type.BLOCK)
+        {
+            closestVanilla = traceVanilla.getPos().squaredDistanceTo(eyesPos);
+            BlockHitResult vanillaHitResult = (BlockHitResult) traceVanilla;
+            side = vanillaHitResult.getSide();
+            closestVanillaPos = vanillaHitResult.getBlockPos();
+        }
+        else if (requireVanillaBlockBehind)
         {
             return null;
         }
 
-        final double closestVanilla = traceVanilla.getPos().squaredDistanceTo(eyesPos);
-
-        BlockHitResult vanillaBlockHit = (BlockHitResult) traceVanilla;
-        Direction side = vanillaBlockHit.getSide();
-        BlockPos closestVanillaPos = vanillaBlockHit.getBlockPos();
         World worldSchematic = SchematicWorldHandler.getSchematicWorld();
         List<BlockHitResult> list = rayTraceBlocksToList(worldSchematic, eyesPos, lookEndPos, RayTraceContext.FluidHandling.NONE, false, false, true, 200);
         BlockHitResult furthestTrace = null;
-        double furthestDist = -1D;
+        double furthestDist = -1.0;
 
         if (list.isEmpty() == false)
         {
@@ -466,8 +475,9 @@ public class RayTraceUtils
             {
                 double dist = trace.getPos().squaredDistanceTo(eyesPos);
 
-                if ((furthestDist < 0 || dist > furthestDist) && (dist < closestVanilla || closestVanilla < 0) &&
-                     trace.getBlockPos().equals(closestVanillaPos) == false)
+                if ((furthestDist < 0 || dist > furthestDist) &&
+                    (dist < closestVanilla || closestVanilla < 0) &&
+                    trace.getBlockPos().equals(closestVanillaPos) == false)
                 {
                     furthestDist = dist;
                     furthestTrace = trace;
@@ -487,7 +497,7 @@ public class RayTraceUtils
         // Note that this method is only used for the "pickBlockLast" type
         // of pick blocking, not for the "first" variant, where this would
         // probably be annoying if you want to pick block the client world block.
-        if (furthestTrace == null)
+        if (furthestTrace == null && side != null && closestVanillaPos != null)
         {
             BlockPos pos = closestVanillaPos.offset(side);
             LayerRange layerRange = DataManager.getRenderLayerRange();
@@ -501,6 +511,56 @@ public class RayTraceUtils
         }
 
         return furthestTrace != null ? furthestTrace.getBlockPos() : null;
+    }
+
+    @Nullable
+    public static RayTraceWrapper getFurthestSchematicWorldTraceBeforeVanilla(World worldClient,
+                                                                              Entity entity,
+                                                                              double maxRange)
+    {
+        Vec3d eyesPos = entity.getCameraPosVec(1f);
+        Vec3d rangedLookRot = entity.getRotationVec(1f).multiply(maxRange);
+        Vec3d lookEndPos = eyesPos.add(rangedLookRot);
+
+        @Nullable BlockPos closestVanillaPos = null;
+        double closestVanilla = -1.0;
+
+        HitResult traceVanilla = getRayTraceFromEntity(worldClient, entity, false, maxRange);
+
+        if (traceVanilla.getType() == HitResult.Type.BLOCK)
+        {
+            closestVanilla = traceVanilla.getPos().squaredDistanceTo(eyesPos);
+            BlockHitResult vanillaHitResult = (BlockHitResult) traceVanilla;
+            closestVanillaPos = vanillaHitResult.getBlockPos();
+        }
+
+        World worldSchematic = SchematicWorldHandler.getSchematicWorld();
+        List<BlockHitResult> list = rayTraceBlocksToList(worldSchematic, eyesPos, lookEndPos, RayTraceContext.FluidHandling.NONE, false, false, true, 200);
+        BlockHitResult furthestTrace = null;
+        double furthestDist = -1.0;
+
+        if (list.isEmpty() == false)
+        {
+            for (BlockHitResult trace : list)
+            {
+                double dist = trace.getPos().squaredDistanceTo(eyesPos);
+
+                if ((furthestDist < 0 || dist > furthestDist) &&
+                    (dist < closestVanilla || closestVanilla < 0) &&
+                    trace.getBlockPos().equals(closestVanillaPos) == false)
+                {
+                    furthestDist = dist;
+                    furthestTrace = trace;
+                }
+
+                if (closestVanilla >= 0 && dist > closestVanilla)
+                {
+                    break;
+                }
+            }
+        }
+
+        return furthestTrace != null ? new RayTraceWrapper(HitType.SCHEMATIC_BLOCK, furthestTrace) : null;
     }
 
     @Nonnull