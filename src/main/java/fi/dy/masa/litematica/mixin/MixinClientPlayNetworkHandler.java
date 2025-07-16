hi5ilay/gitmatica

Paste (command-based): Rewrite most of the main state logic + clean-up
The entity summon commands are now also rate limited properly.

Note that if using the NBT restore option, then the command limit can
overshoot by a couple of commands, since the final commands are
currently not queued and rate limited, but instead just the start
of each block paste checks the current count being below the limit.

src/main/java/fi/dy/masa/litematica/mixin/MixinClientPlayNetworkHandler.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket;
 import net.minecraft.network.packet.s2c.play.ChunkDeltaUpdateS2CPacket;
+import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 import net.minecraft.network.packet.s2c.play.UnloadChunkS2CPacket;
 import net.minecraft.util.math.ChunkSectionPos;
 import fi.dy.masa.litematica.config.Configs;
@@ -46,5 +47,11 @@ public abstract class MixinClientPlayNetworkHandler
         {
             DataManager.getSchematicPlacementManager().onClientChunkUnload(packet.getX(), packet.getZ());
         }
+    }
+
+    @Inject(method = "onGameMessage", at = @At("RETURN"))
+    private void onGameMessage(GameMessageS2CPacket packet, CallbackInfo ci)
+    {
+        DataManager.onChatMessage(packet.getMessage());
     }
 }

xiej2520/litematica

src/main/java/fi/dy/masa/litematica/mixin/MixinClientPlayNetworkHandler.java
@@ -1,5 +1,6 @@
 package fi.dy.masa.litematica.mixin;
 
+import net.minecraft.network.packet.s2c.play.ChatMessageS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -30,5 +31,11 @@ public abstract class MixinClientPlayNetworkHandler
         {
             DataManager.getSchematicPlacementManager().onClientChunkUnload(packet.getX(), packet.getZ());
         }
+    }
+
+    @Inject(method = "onChatMessage", at = @At("RETURN"))
+    private void onGameMessage(ChatMessageS2CPacket packet, CallbackInfo ci)
+    {
+        DataManager.onChatMessage(packet.getMessage());
     }
 }